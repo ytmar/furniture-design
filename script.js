da861f89ec6d4b8a9b0a6e0e5596d324d3a8535b
ytmar <ytmari.dev@gmail.com>
2026-03-15 23:14:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function(){
+$('.burger').click(function(){
+    $('.menu').toggleClass('is-active');
+    $('.burger').toggleClass('is-active');
+    $('.navi-list').toggleClass('is-active');
+});
+
+});
